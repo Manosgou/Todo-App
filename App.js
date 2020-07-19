6b24e06d6111c79356057d459ab9cff53621725e
Manosgou <manolisgouvrikos@gmail.com>
2020-07-19 17:41:55 +0300

    Revert "-Implement realm db in order to store data"
    This reverts commit c89c5bd14494309d42d5ae1ce4b966e3e2dec75d.

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import moment from 'moment';
-import Realm from 'realm';
-import './database.js'
 import { StyleSheet, Text, View, Modal, FlatList} from 'react-native';
 
 import data from './data.js'
@@ -50,15 +48,7 @@ export default class App extends Component {
       }, ...this.state.tasks]
 
     });
-    Realm.open({schema: [TasksListSchema, TaskSchema]})
-    .then(realm => {
-      realm.write(() => {
-        realm.create('TASKS_LIST_SCHEMA', {task});
-      });
-      realm.close();
-    }).catch(error => {
-      console.log(error);
-    });
+    console.log(this.state.tasks)
   };
 
 
